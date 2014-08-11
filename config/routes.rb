ca679f184824865d430ec27cc26d965bc4a8bcf0
alemata <amataloni@gmail.com>
2014-08-11 18:58:05 -0300

    Redirect root to user play

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root :to => redirect('/users/1')
   get "users/:id", :to => "games#user"
   get "users/:id", :to => "games#user"
 
