770803e46342a90a23e733ce67b801fdb4f6545b
alemata <amataloni@gmail.com>
2014-08-11 14:49:01 -0300

    Add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+User.create(name: "User1")
+User.create(name: "User2")
